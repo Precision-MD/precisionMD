a17eae9d377f3a0e7c78579e8a516653372cf1b9
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2024-06-12 17:35:40 -0400

    added nav bar

--- app.py
+++ app.py
@@ -1,3 +1,20 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-st.title("Patient List")
+# horizontal nav-bar
+selected = option_menu(
+    menu_title=None,
+    options=['Home', 'Patient List', 'FAQ'],
+    icons=['house', 'person-lines-fill', 'question-circle'],
+    menu_icon='cast',
+    default_index=0,
+    orientation='horizontal',
+)
+
+# different nav bar pages
+if selected == 'Home':
+    st.title("Home")
+if selected == 'Patient List':
+    st.title("Patient List")
+if selected == 'FAQ':
+    st.title("FAQ")
