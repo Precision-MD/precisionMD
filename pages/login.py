f137e4dd13294134e8c300d213a7198a13d9be2e
jiyapatel4 <jiya.patel@mail.utoronto.ca>
2024-06-16 09:06:50 -0400

    added images folder

--- pages/login.py
+++ pages/login.py
@@ -2,6 +2,10 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 from sqlalchemy.sql import text
 from streamlit_navigation_bar import st_navbar
+import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
+from streamlit_authenticator import Authenticate
 
 
 def show_login():
---
+++ streamlit_app.py
@@ -0,0 +1,76 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from sqlalchemy.sql import text
+from streamlit_navigation_bar import st_navbar
+import uuid
+import os
+import pages as pg
+
+# # *** DATABASE
+# # Create the SQL connection to pets_db as specified in your secrets file.
+# conn = st.connection('patients_db', type='sql')
+
+# # Insert some data with conn.session.
+# with conn.session as s:
+#     s.execute(text('CREATE TABLE IF NOT EXISTS patients (id INTEGER, name TEXT);'))
+#     s.execute(text('DELETE FROM patients;'))
+#     patients = {1000: 'Emily Thompson', 1001: 'David Lee'}
+#     for k in patients:
+#         s.execute(
+#             text('INSERT INTO patients (id, name) VALUES (:id, :name);'),
+#             params=dict(id=k, name=patients[k])
+#         )
+#     s.commit()
+
+# *** IMPORTED NAVIGATION BAR
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+pages = ["Home", "Login", "Patient List", "About Us"]
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(
+    parent_dir, "/Users/jiyapatel/new-streamlit/precisionMD/images/gene_svg.svg")
+styles = {
+    "nav": {
+        "background-color": "#5D7298",
+        "justify-content": "left",
+    },
+    "img": {
+        "padding-right": "14px",
+        "width": "150px",
+        "height": "150px"
+
+    },
+    "span": {
+        "color": "white",
+        "padding": "14px",
+    },
+    "active": {
+        "background-color": "#A6B2C9",
+        "color": "var(--text-color)",
+        "font-weight": "normal",
+        "padding": "14px",
+    }
+}
+options = {
+    "show_menu": False,
+    "show_sidebar": False,
+}
+
+page = st_navbar(
+    pages,
+    logo_path=logo_path,
+    styles=styles,
+    options=options,
+)
+
+functions = {
+    "Login": pg.show_login,
+    "Home": pg.show_home,
+    "Patient List": pg.show_patient_list,
+    "About Us": pg.show_about_us
+}
+go_to = functions.get(page)
+print(go_to)
+if go_to:
+    go_to()
